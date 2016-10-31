585e0130ba29779c03278c8ec85c1c06e7456346
Lndqw12 <COMP222012016@mail.ru>
2016-10-31 16:46:52 +0300

    backbone31/10/16

---
+++ js/main1.js
@@ -0,0 +1,11 @@
+﻿var Person = Backbone.Model.extend({// мясо жареное это класс
+   defaults: {
+       name: 'Pomidor',
+       age: '77',
+       job: 'Kaskader'
+   },
+    walk:function(){
+        return this.get('name')+ ' is walking';
+    }
+});
+//var person = new Person;
