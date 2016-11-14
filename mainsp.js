256594919a9509d6a8b03fcde0f6a16a383e4cdc
Lndqw12 <COMP222012016@mail.ru>
2016-11-14 17:03:19 +0300

    BackBone/p5

--- mainsp.js
+++ mainsp.js
@@ -1,29 +1,24 @@
-$(function) {
+$(function() {
 	window.App = {
 			Models: {},
 			Collections: {},
 			Views: {}
 	};
-	
 	window.template = function(id) {
 		return _.template( $('#' + id).html() );
 	};
-	
 	App.Models.Task = Backbone.Model.extend({});
-	App.Views.Tsk = Backbone.View.exetend({
+	App.Views.Task = Backbone.View.exetend({
 		tagName: 'li',
 		render: function () {
 			this.$el.html( this.model.get('title') );
 			return this;
 	}
 });
-
-var Task = new App.Models.Task({
+var task = new App.Models.Task({
 	title: '—ходить в магаз',
 	priority: 4
 });
 var taskView = new App.Views.Task({ model: task});
-
-console.log (taskView.render().el);
-
+console.log(taskView.render().el);
 });
