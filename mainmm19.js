e18f7ba5001deac29826c24ffb2558371a97b98e
Lndqw12 <COMP222012016@mail.ru>
2016-11-17 17:22:25 +0300

    part11/cool5/lesson17/18/19

---
+++ mainmm19.js
@@ -0,0 +1,28 @@
+﻿(function() {
+	
+	window.App = {
+		Views: {},
+		Models: {},
+		Collecions: {},
+		Router: {}
+	};
+
+	App.Router = Backbone.Router.extend({
+		routes: {
+			'': 'start',
+			'specialTasks/:id' : 'showSpecialTasks'
+		},
+		
+		showSpecialTasks: function (id) {
+			console.log(id);
+		},
+	
+		start: function() {
+			console.log( 'Стартовая' );
+		}
+	});
+
+	new App.Router;
+	Backbone.history.start();
+
+})();
