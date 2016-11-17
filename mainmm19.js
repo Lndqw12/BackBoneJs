6bb3bf5d90dda2d246a0fbb64739d1e16c4b9ec9
Lndqw12 <COMP222012016@mail.ru>
2016-11-17 17:37:21 +0300

    part12/cool6/lesson17/18/19

--- mainmm19.js
+++ mainmm19.js
@@ -6,6 +6,18 @@
 		Collecions: {},
 		Router: {}
 	};
+	
+	var vent = _.extend({}, Backbone.Events);
+	
+	//console.log(vent);
+	App.Views.SpecialTask = Backbone.View.extend({
+		initialize: function () {
+			vent.on('specialTasks:show', this.show, this);
+		},
+		show:function (id) {
+			console.log('Выведем задачу с id : ' + id);
+		}
+	});
 
 	App.Router = Backbone.Router.extend({
 		routes: {
@@ -14,14 +26,17 @@
 		},
 		
 		showSpecialTasks: function (id) {
-			console.log(id);
+			vent.trigger('specialTasks:show', id);
+			//console.log(id);
 		},
 	
 		start: function() {
 			console.log( 'Стартовая' );
 		}
 	});
-
+	
+	new App.Views.SpecialTask;
+	
 	new App.Router;
 	Backbone.history.start();
 
