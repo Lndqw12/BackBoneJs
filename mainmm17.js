e3a2aad2e26c1380f08a30a5ee328f1bd0077d01
Lndqw12 <COMP222012016@mail.ru>
2016-11-17 15:32:49 +0300

    part1

--- mainmm17.js
+++ mainmm17.js
@@ -1,13 +1,27 @@
 (function() {
 	
 	window.App = {
-		Views: {}
+		Views: {},
 		Models: {},
-		Collecions: {}
+		Collecions: {},
+		Router: {}
 };
 //шаблончик
 window.template = function(id) {
 	return _.template( $('#' + id).hrml() );
 };
 
-}) ();
+App.Router = Backbone.Router.extend({
+	reuters: {
+		'' : 'index'
+	},
+	
+	index: function() {
+		console.log('Running');
+	}
+});
+
+new App.Router();
+
+
+})();
