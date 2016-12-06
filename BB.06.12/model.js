095afa59393acc4658ca5707c5225200c7859f42
Lndqw12 <COMP222012016@mail.ru>
2016-12-06 17:17:26 +0300

    bb5,6

--- BB.06.12/model.js
+++ BB.06.12/model.js
@@ -3,7 +3,19 @@ var app = app || {};
 $(function () {
 
     app.MyObject = Backbone.Model.extend({
-
+        defaults: {
+            name: "name",
+            description: "-",
+            size: 100
+        },
+        initialize: function() {
+            console.log('obj create');
+            this.on('change', function (){
+                console.log('obj change');
+                var json  = app.myObect.toJSON();
+                console.log(json);
+            });
+        }
     });
 
     app.myObect = new app.MyObject({
@@ -12,7 +24,15 @@ $(function () {
         class: "grenade"
     });
 
+    app.myObect.set({
+        type: "active",
+        size: 250
+    });
+
+    //console.log ("size: "+app.MyObject.get('size'));
+
     var json  = app.myObect.toJSON();
-    console.log(json);
+    //console.log(json);
+    //console.log ('size:', 'size:');
 
 });
